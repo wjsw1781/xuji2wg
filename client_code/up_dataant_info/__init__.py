ed238e6aa49855cd314384b83701c64943c73fdb
2823558582@qq.com <2823558582@qq.com>
2025-10-21 03:37:18 +0000

    Created up_dataant_info.RowTemplate1 and up_dataant_info; Edited settings; Deleted Form1

--- client_code/up_dataant_info/__init__.py
+++ client_code/up_dataant_info/__init__.py
@@ -1,7 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import up_dataant_infoTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class up_dataant_info(up_dataant_infoTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
