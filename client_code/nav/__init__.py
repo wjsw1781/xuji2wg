ab06f8a1b99541291d4c74bee7d64be85c7d54da
2823558582@qq.com <2823558582@qq.com>
2025-10-21 09:17:41 +0000

    Created job, job.RowTemplate1, job.RowTemplate2, nav and 1 more; Edited settings; Deleted up_dataant_info.RowTemplate1, up_dataant_info and up_dataant_info.RowTemplate2

---
+++ client_code/nav/__init__.py
@@ -0,0 +1,23 @@
+from ._anvil_designer import navTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class nav(navTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+
+    def primary_color_2_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        open_form('node')
+
+    def primary_color_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        open_form('job')
+
+        
---
+++ client_code/node/__init__.py
@@ -0,0 +1,60 @@
+from ._anvil_designer import nodeTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil
+
+from ..utils import *
+
+
+class node(nodeTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        # Any code you write here will run before the form opens.
+        self.grid = nearest_datagrid(self)
+        self.repeat = self.grid.get_components()[1]
+        self.table_name = find_table_exact(self.grid, all_table_scahma)
+        self.table_obj = getattr(app_tables, self.table_name)
+
+        self.repeat.items = list(self.table_obj.search())
+
+        self.rows = self.repeat.items
+        self.dropdowns = {}  # {字段名: DropDown}
+
+        if self.rows:
+            filter_row = FlowPanel()
+            for col in dict(self.rows[0]).keys():  # 每一个字段
+                dd = DropDown(placeholder=col)
+                dd.items = [("全部", None)] + [  # 去重后的唯一值
+                    (str(v), v)
+                    for v in sorted({r[col] for r in self.rows if r[col] is not None})
+                ]
+                filter_row.add_component(dd)
+                self.dropdowns[col] = dd
+            self.add_component(filter_row)
+
+    def add_one_row_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        # 获取当前数据库表的名称和字段
+
+        row = quick_add_row(self.grid)
+
+        if not row:
+            return
+
+        self.table_obj.add_row(**row)
+
+        self.repeat.items = list(self.table_obj.search())
+
+    def query_click(self, **event_args):
+        cond = {
+            c: dd.selected_value
+            for c, dd in self.dropdowns.items()
+            if dd.selected_value is not None
+        }  # 只取被选择的字段
+
+        self.repeat.items = list(
+            self.table_obj.search(**cond) if cond else self.table_obj.search()
+        )
