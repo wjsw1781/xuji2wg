67a772dcc99fad17f9205cd27d5f47ec52df674d
2823558582@qq.com <2823558582@qq.com>
2025-10-21 10:20:13 +0000

    Created LICENSE.txt; Edited settings and server_code

--- server_code/server_code.py
+++ server_code/server_code.py
@@ -14,14 +14,15 @@ import anvil.server
 # def say_hello(name):
 #   print("Hello, " + name + "!")
 #   return 42
-#
-# @anvil.server.route('/add',methods=['post'])
+
+
+
+
 @anvil.server.http_endpoint("/add", methods=["POST","GET"], authenticate_users=False)
 def wg_server_public_ip_update(**kw):
+    print(kw)
     data = anvil.server.request.body_json      # 这里拿到 JSON 数据
     return data
-
-
 
 # @anvil.server.http_endpoint("/add_table_json", methods=["POST","GET"], authenticate_users=False)
 # def update_table_json(**kw):
