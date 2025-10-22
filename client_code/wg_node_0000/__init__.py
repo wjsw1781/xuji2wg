c8de5ecf130d30964123ef6fa5e3948988fca201
2823558582@qq.com <2823558582@qq.com>
2025-10-22 14:25:53 +0000

    Created FilterBar; Edited wg_node_0000

--- client_code/wg_node_0000/__init__.py
+++ client_code/wg_node_0000/__init__.py
@@ -9,7 +9,7 @@ import anvil
 from ..utils import *
 
 
-
+from ..FilterBar import FilterBar
 
 class wg_node_0000(wg_node_0000Template):
     def __init__(self, **properties):
@@ -26,6 +26,14 @@ class wg_node_0000(wg_node_0000Template):
 
         self.rows = self.repeat.items
 
+        # 头部筛选数据框
+        self.add_component(FilterBar(self.rows, self.update_table), index=0)
+        self.update_table(self.rows)
+ 
+
+    def update_table(self, filtered_rows):
+        self.repeat.items = filtered_rows  
+        
         # 样式控制 文字单元格太长都省略掉
         for row_tpl in self.repeat.get_components():
             row_comps = row_tpl.get_components()
@@ -37,7 +45,7 @@ class wg_node_0000(wg_node_0000Template):
                     continue
 
                 node = anvil.js.get_dom_node(comp).querySelector("span")
-                
+
                 # 文本处理
                 node.style.whiteSpace = "nowrap"
                 node.style.overflow = "hidden"
@@ -49,7 +57,7 @@ class wg_node_0000(wg_node_0000Template):
                 text = node.innerHTML or ""
                 if 'http' in text:
                     node.innerHTML = f'<a href="{text}" target="_blank" >{text}</a>'
-                    
+
                 # 图片处理
                 if 'img' in (col_info.get('data_key') or ''):
                     b64 = (comp.text or "").strip()
@@ -63,44 +71,4 @@ class wg_node_0000(wg_node_0000Template):
                     # 3) 用同一列位置替换组件
                     row_tpl.add_component(btn, column=col_info['id'])
                     comp.remove_from_parent()  
-   
-        
-            
 
-        # 产生自动下拉框
-        self.dropdowns = {}  # {字段名: DropDown}
-        if self.rows:
-            filter_row = FlowPanel()
-            for col in dict(self.rows[0]).keys():  # 每一个字段
-                dd = DropDown(placeholder=col,width=100)
-                dd.items = [("全部", None)] + [  # 去重后的唯一值
-                    (str(v), v)
-                    for v in sorted({r[col] for r in self.rows if r[col] is not None})
-                ]
-                filter_row.add_component(dd)
-                self.dropdowns[col] = dd
-            self.add_component(filter_row,index = 0)
-
-    def add_one_row_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        # 获取当前数据库表的名称和字段
-
-        row = quick_add_row(self.grid)
-
-        if not row:
-            return
-        self.table_obj.add_row(**row)
-        self.table_obj.list_columns()
-
-        self.repeat.items = list(self.table_obj.search())
-
-    def query_click(self, **event_args):
-        cond = {
-            c: dd.selected_value
-            for c, dd in self.dropdowns.items()
-            if dd.selected_value is not None
-        }  # 只取被选择的字段
-
-        self.repeat.items = list(
-            self.table_obj.search(**cond) if cond else self.table_obj.search()
-        )
