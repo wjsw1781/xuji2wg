8882b90059cc955b9253f001f74f308144457448
pak.wzq <pak.wzq@alibaba-inc.com>
2025-10-22 15:08:35 +0800

    水电费

--- crud/crud_增加网络节点0000.py
+++ crud/crud_增加网络节点0000.py
@@ -9,7 +9,16 @@ import subprocess, time,os,sys,json,random,ipaddress
 from loguru import logger
 
 
-from crud import *
+url_host = 'http://8.217.224.52:59001'
+
+crud_c_url = f'{url_host}/c_table_json/'
+crud_r_url = f'{url_host}/r_table_json/'
+crud_u_url = f'{url_host}/u_table_json/'
+crud_d_url = f'{url_host}/d_table_json/'
+
+import requests
+
+
 table = 'wg_node_0000'
 
 
