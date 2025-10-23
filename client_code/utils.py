c6a53813dbbc46adc07f6c2bf4daf82c30ab0460
2823558582@qq.com <2823558582@qq.com>
2025-10-23 09:37:13 +0000

    Edited imei_node, job, wg_node_0000_status and utils

--- client_code/utils.py
+++ client_code/utils.py
@@ -144,7 +144,16 @@ def list_add_self_items(self):
         self.repeat.items = cache_data[table_name]
         return
         
-    # 进行网络请求
-    self.repeat.items  = list(map(dict,self.table_obj.search()))
+    # 进行网络请求     # 改成进度展示
+
+    total = len(app_tables.imei_node.search({}))
+    data = []
+    index = 0
+    for i in self.table_obj.search():
+        data.append(dict(i))
+        index += 1
+        if index%100 == 0:
+            Notification(f'进度 {index} / {total}')
+    self.repeat.items  = list(reversed(data))
     cache_data[table_name] = self.repeat.items 
     pass
