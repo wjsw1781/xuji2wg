ecbe7a1958e5196fc36baf329935838afe968764
pak.wzq <pak.wzq@alibaba-inc.com>
2025-10-23 23:19:35 +0800

    fix

--- server_code/server_code.py
+++ server_code/server_code.py
@@ -94,5 +94,5 @@ def delete_table_json(**kw):
 # 自定义路由 显示 1097 下面的一些节点
 @anvil.server.route("/wg_node_0000_by_route", methods=["POST","GET"], authenticate_users=False)
 def wg_node_0000_by_route(**kw):
-    return anvil.server.FormResponse('MyPageForm',**kw)
+    return anvil.server.FormResponse('wg_node_0000',**kw)
 
